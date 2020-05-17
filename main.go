1df1aaca7c8e8b5feaf7186815734410cc4ff73f
Roneet Kumar <roneetparida@gmail.com>
2020-05-17 03:43:21 -0400

    custom response writer

--- main.go
+++ main.go
@@ -17,7 +17,6 @@ func main() {
 
 func recoverMw(app http.Handler, dev bool) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
 		defer func() {
 			if err := recover(); err != nil {
 				log.Println(err)
@@ -35,8 +34,49 @@ func recoverMw(app http.Handler, dev bool) http.HandlerFunc {
 			}
 		}()
 
-		app.ServeHTTP(w, r)
+		// new response writer
+
+		nw := &responseWrite{ResponseWriter: w}
+
+		app.ServeHTTP(nw, r)
+		nw.flush()
 	}
+}
+
+// type ResponseWriter interface {
+// 	Header() Header
+// 	Write([]byte) (int, error)
+// 	WriteHeader(statusCode int)
+// }
+
+type responseWrite struct {
+	http.ResponseWriter
+	writes [][]byte
+	status int
+}
+
+func (rw *responseWrite) Write(b []byte) (int, error) {
+	rw.writes = append(rw.writes, b)
+	return len(b), nil
+}
+
+func (rw *responseWrite) WriteHeader(statusCode int) {
+
+	rw.status = statusCode
+}
+
+func (rw *responseWrite) flush() error {
+	if rw.status != 0 {
+
+		rw.ResponseWriter.WriteHeader(rw.status)
+	}
+	for _, write := range rw.writes {
+		_, err := rw.ResponseWriter.Write(write)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func panicDemo(w http.ResponseWriter, r *http.Request) {
